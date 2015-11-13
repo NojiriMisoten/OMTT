47d0708f3e5b41bcc8df679873348a9878217afb
satoryoichi <satoryohal@gmail.com>
2015-11-13 15:50:24 +0900

    コマンドチャート変更点：コマンド情報追加、入力コマンドチェック、コマンドが入力されていた場合どのコマンドが入力されているか返す

--- kami_puro/Sources/SCENE/GAME/COMMANDCHART/CCommandChart.h
+++ kami_puro/Sources/SCENE/GAME/COMMANDCHART/CCommandChart.h
@@ -27,18 +27,28 @@ static const int MAX_PLAYER = 2;
 // コマンドの種類
 typedef enum
 {
-	COMMAND_TYPE_NONE = 0,
-	COMMAND_TYPE_CHOP,			// チョップ
+	COMMAND_TYPE_CHOP = 0,		// チョップ
 	COMMAND_TYPE_ELBOW,			// エルボー
 	COMMAND_TYPE_LARIAT,		// ラリアット
 	COMMAND_TYPE_ROLLING_ELBOW,	// ローリングエルボー
 	COMMAND_TYPE_FLYING_ELBOW,	// フライングエルボー
 	COMMAND_TYPE_DROP_KICK,		// ドロップキック
+	COMMAND_TYPE_FACE_SLAPPING,	// ビンタ
+	COMMAND_TYPE_BACKDROP,		// バックドロップ
 	COMMAND_TYPE_STANER,		// スタナー
 	COMMAND_TYPE_ROPE,			// ロープ
 	COMMAND_TYPE_FINISHER,		// 決め技
-	COMMAND_TYPE_MAX
+	COMMAND_TYPE_MAX,			// 技の数
+	COMMAND_TYPE_NONE			// コマンドが不正だった時
 }COMMAND_TYPE;
+
+// コマンド情報
+typedef struct
+{
+	const int s_nCommandLength;
+	const COMMAND_TYPE s_CommandType;
+	const BUTTON_TYPE* s_Command;
+}COMMAND_INFO;
 
 //-----------------------------------------------------------------------------
 //	前方宣言
@@ -75,20 +85,20 @@ public:
 	// 戻り値：自身のポインタ、プレイヤー番号
 	static CCommandChart* Create(LPDIRECT3DDEVICE9* pDevice, int nID);
 
-	// 次に入力する候補のコマンドの表示
-	// 引数：何個目のコマンド表記か
-	void CreateNextCommand(int nNumCommand);
-
-	// 次に表示するコマンドを削除
-	void DeathNextCommand(void);
-
 	// 画面外へのフェードアウト
 	void ScreenOut(void);
 
 	// 画面内へのフェードイン
 	void ScreenIn(void);
 
+	// 最終的に繰り出す技を調べて返す
+	// 戻り値：繰り出す技のタイプ
+	COMMAND_TYPE UseTechnic(void);
+
 private:
+	//*************************************
+	// 関数
+	//*************************************
 	// デフォルトの状態に変数を戻す
 	void SetDefault(void);
 
@@ -98,6 +108,28 @@ private:
 	// コマンドのリセット
 	void ResetCommand(void);
 
+	// 次に表示するコマンドを削除
+	void DeathNextCommand(void);
+
+	// 一番初めに表示する入力候補のコマンドの作成
+	void CreateFirstCommand(void);
+	
+	// 次に入力する候補のコマンドのチェック
+	// 戻り値：入力候補となるコマンド情報の配列のポインタ
+	COMMAND_INFO* NextCommandCheck(void);
+
+	// 次に入力する候補のコマンドの作成
+	// 引数：何個目のコマンド表記か
+	void CreateNextCommand(int nNumCommand);
+
+	// コマンドのチェック
+	// 引数：技候補の構造体の配列
+	// 戻り値：コマンドが正しかったか否か
+	bool CheckCommand(COMMAND_INFO* Technic);
+
+	//*************************************
+	// 変数
+	//*************************************
 	// コマンド保持用配列
 	BUTTON_TYPE m_aCommandKeep[MAX_COMMAND_KEEP];
 	// デバイスの保持
