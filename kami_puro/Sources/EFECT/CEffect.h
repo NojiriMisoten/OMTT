6654ece16cb2ef1695ef2316962850408a75ded5
sakamotokun <sw.style0312@gmail.com>
2015-12-05 17:19:35 +0900

    エフェクトに速度関連追加 Createの時の引数にも追加してGet関数とSet関数も追加しました

--- kami_puro/Sources/EFECT/CEffect.h
+++ kami_puro/Sources/EFECT/CEffect.h
@@ -42,7 +42,7 @@ class CEffect : public CScene
 
 		//クリエイト
 		static CEffect* Create(int maxFrame, EFFECT_TYPE filename, bool isloop_);
-		static CEffect* Create(int maxFrame, EFFECT_TYPE filename, bool isloop_, D3DXVECTOR3& pos, D3DXVECTOR3& rot, D3DXVECTOR3& scl);
+		static CEffect* Create(int maxFrame, EFFECT_TYPE filename, bool isloop_, D3DXVECTOR3& pos, D3DXVECTOR3& rot, D3DXVECTOR3& scl, D3DXVECTOR3& moveSpd = D3DXVECTOR3(0, 0, 0), D3DXVECTOR3& rotSpd = D3DXVECTOR3(0, 0, 0));
 		
 		// ポジションアクセサ
 		D3DXVECTOR3& GetPos( void ){ return m_Pos; };
@@ -55,6 +55,18 @@ class CEffect : public CScene
 		D3DXVECTOR3& GetRot( void ){ return m_Rot; };
 		void		SetRot( D3DXVECTOR3& rot ){ m_Rot = rot; };
 		void		SetRot( float x, float y, float z ){ m_Rot.x = x; m_Rot.y = y; m_Rot.z = z; };
+
+		// ポジション速度アクセサ
+		D3DXVECTOR3& GetPosSpd(void){ return m_Move_Spd; };
+		D3DXVECTOR3* GetPosSpdAdr(void){ return &m_Move_Spd; };
+		void AddPosSpd(D3DXVECTOR3& posSpd){ m_Move_Spd += posSpd; }
+		void		SetPosSpd(D3DXVECTOR3& posSpd){ m_Move_Spd = posSpd; };
+		void		SetPosSpd(float x, float y, float z){ m_Move_Spd.x = x; m_Move_Spd.y = y; m_Move_Spd.z = z; };
+
+		// ロット速度アクセサ
+		D3DXVECTOR3& GetRotSpd(void){ return m_Rot_Spd; };
+		void		SetRotSpd(D3DXVECTOR3& rotSpd){ m_Rot_Spd = rotSpd; };
+		void		SetRotSpd(float x, float y, float z){ m_Rot_Spd.x = x; m_Rot_Spd.y = y; m_Rot_Spd.z = z; };
 
 		// スケールアクセサ
 		D3DXVECTOR3& GetScl( void ){ return m_vScl; };
@@ -97,6 +109,9 @@ class CEffect : public CScene
 	private:
 		// 大きさ（スケール）
 		D3DXVECTOR3         m_vScl;
+		//各スピード
+		D3DXVECTOR3         m_Move_Spd;
+		D3DXVECTOR3         m_Rot_Spd;
 		//ファイル名
 		EFFECT_TYPE         EffectType;
 		EFFECT_TYPE         NextEffectType;
